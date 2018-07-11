f228a601b5546c6cb2f9168bcb2684a11e2e12f1
Deep <deepdeep.ac@gmail.com>
2018-07-11 13:17:29 -0400

    final

--- piToNth_bbp.py
+++ piToNth_bbp.py
@@ -4,7 +4,7 @@ def piToNth(num): #Bailey–Borwein–Plouffe-algorithm
     pi = Decimal(0)
     k = 0
     while k < num:
-        pi += (((4/(8*k + 1))-(2/(8*k + 4))-(1/(8*k + 5))-(1/(8*k + 6))))*16 **(-k)
+        pi += ((Decimal(4/(8*k + 1))-Decimal(2/(8*k + 4))-Decimal(1/(8*k + 5))-Decimal(1/(8*k + 6))))* Decimal(16 **(-k))
         k += 1
     return pi 
 num = Decimal(input("Give the position till which you want the Pi to be tracked: "))
