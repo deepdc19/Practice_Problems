433cf36b7568f28c8bdc9246efe881f41e186d9f
Deep <deepdeep.ac@gmail.com>
2018-07-11 13:13:09 -0400

    the rename

---
+++ piToNth_chudnovsky.py
@@ -0,0 +1,22 @@
+from decimal import Decimal
+from decimal import getcontext
+def factorial(n):
+    if n < 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def piToNth(num):
+    pi = Decimal(0)
+    k = 0
+    while k < num:
+        pi += (Decimal(-1)**(k) * Decimal(factorial(6*k)) * Decimal((545140134*k + 13591409))) / (Decimal(factorial(3*k)) * Decimal((factorial(k)**3)) * Decimal((640320)**((3*k+3)/2)))
+        k = k + 1 
+    pi = (pi ** Decimal(-1)) / Decimal(12) 
+    return pi 
+num = Decimal(input("Give the position till which you want the Pi to be tracked: "))
+getcontext().prec = int(num)
+print(piToNth(num))
+
+
+    
