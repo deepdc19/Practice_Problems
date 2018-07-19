505e50083d8c54f31bc73a3ce2a14bb9d4ecef20
Deep <deepdeep.ac@gmail.com>
2018-07-19 16:01:52 -0400

    The first function

--- the_tree.py
+++ the_tree.py
@@ -1,1 +1,40 @@
+from collections import deque
+
+class TreeNode():
+    '''
+    TreeNode represents the binary tree node
+
+    '''
+    def __init__(self):
+        self.value = value
+        self.left = None  
+        self.right = None
+
+class Binarytree():
+    '''
+    Represent Binary tree and methods on it.
+
+    '''
+    def __init__(self, root = None):
+        self.root = None
+    
+    def iterative_in_order_LNR(self):
+        final_order = []
+        current = self.root
+        temp_order = []
+        while temp_order or current:
+            if current:
+                print("current", current)
+                temp_order.append(current)
+                current = current.left
+            else:
+                current = temp_order.pop()
+                final_order.append(current)
+                current = current.right 
+        return final_order
+
+
+
+
+
 
